daeb798f65e933be66937df026ff4c4a24407eec
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-02-19 17:40:54 -0300

    :white_check_mark: tests: add unit testing (#40)
    add basic testing
    
    See issues: 40

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -2,11 +2,11 @@ import * as minify from 'minify'; // just ignore if an error appers
 import { readFileSync, unlinkSync, writeFileSync } from 'node:fs';
 
 (async () => {
-  const DIST_FILE = `./dist/TickSync.js`;
+  const DIST_FILE = `./dist/GcalSync.js`;
 
   const packageJson = JSON.parse(readFileSync('./package.json', { encoding: 'utf8' }));
   const distFileContent = readFileSync(DIST_FILE, { encoding: 'utf8' });
-  const distFileWithVersion = distFileContent.replace(`this.version = ''; // version`, `this.version = '${packageJson.version}'; // version`);
+  const distFileWithVersion = distFileContent.replace(`VERSION = ''; // version`, `VERSION = '${packageJson.version}'; // version`);
   writeFileSync(DIST_FILE, distFileWithVersion);
 
   await minifyFile(DIST_FILE);
